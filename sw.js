3920b775f7a9ed1f9abea1611c1e9b715ff68d64
yrds03 <yohanesrikkydwisantoso@gmail.com>
2026-07-21 09:38:50 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,28 @@
-const CACHE_NAME = 'sanstech-pwa-v4';
+const CACHE_NAME = 'sanstech-pwa-v5';
 const urlsToCache = [
     './',
     './index.html',
-    './manifest.json',
-    './logo.png'
+    './manifest.json'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
+            .then(cache => cache.addAll(urlsToCache))
+            .catch(err => console.log('Abaikan jika gagal cache:', err)) // Anti-crash
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) return response;
-                return fetch(event.request);
-            })
+        caches.match(event.request).then(response => response || fetch(event.request))
     );
 });
 
 self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        ))
     );
 });
