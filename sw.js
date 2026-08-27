4701e499286c90e43173a8179e35cc3cafaf52b2
yrds03 <yohanesrikkydwisantoso@gmail.com>
2026-08-27 20:24:34 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sanstech-erp-v15'; // Ganti versi jika ada update besar
+const CACHE_NAME = 'sanstech-erp-v16'; // Ganti versi jika ada update besar
 const urlsToCache = [
   '/',
   '/index.html',
