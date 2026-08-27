30bc1bf21a0c6ccd7aaba8a94cfb8984559d9cab
yrds03 <yohanesrikkydwisantoso@gmail.com>
2026-08-27 18:34:50 +0700

    Update service worker for new cache version and logic

--- sw.js
+++ sw.js
@@ -1,28 +1,47 @@
-const CACHE_NAME = 'sanstech-pwa-v10';
+const CACHE_NAME = 'sanstech-erp-v11'; // Ganti versi jika ada update besar
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json'
+  '/',
+  '/index.html',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-            .catch(err => console.log('Abaikan jika gagal cache:', err)) // Anti-crash
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
 
 self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-        ))
-    );
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// WAJIB ADA: Menerima perintah untuk segera mengaktifkan SW yang baru
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
